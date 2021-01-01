57182e3f94dc6fe862d9f877075af7ef4339fb88
zbelial <zjytj@qq.com>
2021-01-01 11:55:45 +0800

    remove a require

--- shrface.el
+++ shrface.el
@@ -7,7 +7,7 @@
 ;; Keywords: faces
 ;; Created: 10 April 2020
 ;; Version: 2.6
-;; Package-Requires: ((emacs "25.1") (org "9.0"))
+;; Package-Requires: ((emacs "25.1"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
